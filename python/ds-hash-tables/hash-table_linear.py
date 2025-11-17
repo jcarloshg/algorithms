c9106ac1a2ee7ca10a92a529a66688717bd435e1
jcarloshg <carlosj12336@gmail.com>
2025-11-17 13:40:47 -0600

    add hast tables

--- python/ds-hash-tables/hash-table_linear.py
+++ python/ds-hash-tables/hash-table_linear.py
@@ -4,23 +4,24 @@ class HashTableLinear:
         self.last_index = size - 1
         self._table = {}
         pass
-    
+
     def get(self, key: int) -> str | None:
         key_str = str(key)
         return self._table.get(key_str)
-    
-    def _hash(self, obj:str) -> int :
-        
+
+    def _hash(self, obj: str) -> int:
+
         # create a key
-        total_sum=0
+        total_sum = 0
         for char in obj:
             char_unicode = ord(char)
             total_sum += char_unicode
-        
+
+        # the key must be major than 0 and lower than last_index
         key = total_sum
         while key < 0 or key > self.last_index:
             key = total_sum % self.size
-        
+
         # validate that position is free
         temp_key = key
         is_there_data = self._table.get(key) is not None
@@ -28,21 +29,23 @@ class HashTableLinear:
             key = (key + 1) % self.size
             if key == temp_key:
                 is_there_data = False
-        
+
         return key
-    
-    def insert(self, obj:str):
+
+    def insert(self, obj: str):
         key = self._hash(obj)
         self._table[key] = obj
         # print(f"obj '{obj}' was inserted at [{key}]")
-    
+
     def print(self):
         print(f"self._table {self._table}")
         # for item_data in enumerate(self._table):
         #     print(f"item_data {item_data}")
-            
 
-        
+    def keys(self) -> list:
+        return list(self._table.keys())
+
+
 hashTableLinear = HashTableLinear(30)
 food_names = [
     "apple", "banana", "carrot", "bread", "cheese", "chicken", "rice", "beans",
@@ -53,4 +56,7 @@ for name in food_names:
     hashTableLinear.insert(name)
     hashTableLinear.print()
 
+
 hashTableLinear.print()
+keys = hashTableLinear.keys()
+print(f"keys {keys}")
