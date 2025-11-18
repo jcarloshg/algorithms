7960585229fb3eef09b70404be541c9628c3859f
jcarloshg <carlosj12336@gmail.com>
2025-11-17 21:56:43 -0600

    add linked list

---
+++ python/ds-linked-list/info.md
@@ -0,0 +1,19 @@
+# 🧩 Linked List Summary
+
+> A **linked list** is a linear data structure where elements (called **nodes**) are connected using pointers. Each node contains:
+>
+> - **Data**
+> - **Reference** to the next node in the sequence
+
+## 🚀 Key Features
+
+- Efficient **insertion** and **deletion** of elements (especially compared to arrays)
+- **Slower access by index** (requires traversal)
+
+## 📚 Types of Linked Lists
+
+| Type     | Description                                      |
+| -------- | ------------------------------------------------ |
+| Singly   | Each node points to the next node                |
+| Doubly   | Each node points to both next and previous nodes |
+| Circular | Last node points back to the first node          |
---
+++ python/ds-linked-list/linked-lis.py
@@ -0,0 +1,104 @@
+class Node:
+
+    def __init__(self, value):
+        self._value = value
+        self._next = None
+
+    def set_next(self, item: 'Node'):
+        self._next = item
+
+    def get_next(self) -> 'Node | None':
+        return self._next
+
+    def get_value(self):
+        return self._value
+
+
+class LinkedList:
+
+    def __init__(self):
+        self._head = None
+        self._tail = None
+        self._lenght = 0
+
+    def apped(self, value):
+
+        new_node = Node(value)
+
+        if self._tail is None and self._head is None:
+            self._tail = new_node
+            self._head = new_node
+            return
+
+        self._head.set_next(new_node)
+        self._head = new_node
+        self._lenght += 1
+
+    def prepend(self, value):
+
+        new_node = Node(value)
+
+        if self._tail is None and self._head is None:
+            self._tail = new_node
+            self._head = new_node
+            return
+
+        new_node.set_next(self._tail)
+        self._tail = new_node
+        self._lenght += 1
+
+    def insert(self, index: int, value):
+
+        if index > self._lenght:
+            self.apped(value)
+            return
+
+        if index < 0:
+            return
+
+        if index == 0:
+            self.prepend(value)
+            return
+
+        prev_node = self.get_by_index(index-1)
+        if not prev_node:
+            return
+
+        new_node = Node(value=value)
+        next_for_prev = prev_node.get_next()
+        prev_node.set_next(new_node)
+        new_node.set_next(next_for_prev)
+        self._lenght += 1
+
+    def get_by_index(self, index: int):
+        aux_index = 0
+        aux_pivot = self._tail
+        while aux_index < index:
+            aux_index += 1
+            aux_pivot = aux_pivot.get_next()
+        return aux_pivot
+
+    def print(self):
+
+        visiter = self._tail
+        i = 0
+
+        while visiter:
+            value = visiter.get_value()
+            print(f"[{i}] -> {value}")
+            visiter = visiter.get_next()
+            i += 1
+        print("\n")
+
+
+linkedList = LinkedList()
+linkedList.apped("jose")
+linkedList.apped("carlos")
+linkedList.apped("huerta")
+linkedList.print()
+
+linkedList.prepend("garcia")
+linkedList.print()
+
+linkedList.insert(2, "andree")
+linkedList.print()
